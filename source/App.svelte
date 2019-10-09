7b531c3cc05f00352c36891c9b6adc29b921e6a4
Artem Demo <artemdemo@gmail.com>
2019-10-09 17:37:16 +0300

    moved css norm styling to App

--- source/App.svelte
+++ source/App.svelte
@@ -1,7 +1,6 @@
 <script>
     // import 'tailwindcss/dist/base.min.css';
     // import 'tailwindcss/dist/utilities.min.css';
-    import './styles/normalization.css';
 
     import Button from './components/Button';
     import { buttonAppearance } from './components/ButtonProps';
@@ -15,6 +14,42 @@
     let uuidResult = uuid();
 
 </script>
+
+<style>
+    /* Global normalization */
+    :global(*), :global(::after), :global(::before) {
+        border-width: 0;
+        border-style: solid;
+        border-color: #e2e8f0;
+    }
+
+    :global(body) {
+        margin: 0;
+    }
+
+    :global(html) {
+        line-height: 1.5;
+        box-sizing: border-box;
+    }
+
+    :global(button),
+    :global(input),
+    :global(optgroup),
+    :global(select),
+    :global(textarea) {
+        padding: 0;
+        line-height: inherit;
+        color: inherit;
+        font-family: inherit;
+        font-size: 100%;
+        margin: 0;
+    }
+
+    :global(button) {
+        background: 0 0;
+        padding: 0;
+    }
+</style>
 
 <Container>
     <Section>
