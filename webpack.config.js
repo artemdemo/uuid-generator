81de17f8e844f6bd900d05e6069fe4bb02bfeeb7
Artem Demo <artemdemo@gmail.com>
2020-07-03 14:48:06 +0300

    fixed typo

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         alias: {
             svelte: path.resolve('node_modules', 'svelte')
         },
-        extensions: ['.mjs', '.js', '.svelte'],
+        extensions: ['.js', '.svelte'],
         mainFields: ['svelte', 'browser', 'module', 'main']
     },
     output: {
